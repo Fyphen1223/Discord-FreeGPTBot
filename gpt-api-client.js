35c53ef3c05a68a244f8d499924306559cf4457a
Fyphen <fyphensub@gmail.com>
2023-12-11 13:58:06 +0900

    Create gpt-api-client.js

---
+++ gpt-api-client.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+
+async function generate(prompt, model) {
+	let res = await axios({
+		method: "post",
+		url: `https://gpti.projectsrpp.repl.co/api/gpti`,
+		headers: {
+			"Content-Type": "application/json",
+		},
+		data: {
+			prompt: prompt,
+			model: model,
+			type: "json",
+		},
+	});
+	if (res.data.code !== 200) {
+		res = await axios({
+			method: "post",
+			url: `https://gpti.projectsrpp.repl.co/api/gpti`,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: {
+				prompt: prompt.slice(-20000),
+				model: model || "gpt-4-32k",
+				type: "json",
+			},
+		});
+		if (res.data.code !== 200) {
+			throw new Error('Axios failed to fetch. It might be an API issue.');
+		}
+
+	}
+	return res.data.gpt;
+}
+
+module.exports = { generate };
